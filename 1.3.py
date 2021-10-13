404130cd29d4ede4e81fae9ca68e98815a4ea66c
TonkoshkuraMisha <tonkoshkura.misha@gmail.com>
2021-10-14 00:01:03 +0300

    files 1.3 and 1.4 was added

---
+++ 1.3.py
@@ -0,0 +1,7 @@
+class Car:
+    model = "BMW"
+    engine = 1.6
+
+
+a1 = Car()
+a2 = Car()
---
+++ 1.4.py
@@ -0,0 +1,7 @@
+class Car:
+    model = "Renault"
+    engine = 1.6
+
+    @staticmethod
+    def drive():
+        print("Let`s go!")
