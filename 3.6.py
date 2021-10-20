c3710ecc108a61a8ff08aa9d9e997581b4ff86eb
TonkoshkuraMisha <tonkoshkura.misha@gmail.com>
2021-10-20 16:18:05 +0300

    3.6 file was changed

--- 3.6.py
+++ 3.6.py
@@ -12,3 +12,26 @@ class Point:
     def __bool__(self):
         print('__bool__')
         return self.x != 0 or self.y != 0
+
+class City:
+    def __init__(self, city):
+        self.name = city.title()
+    def __str__(self):
+        return self.name
+    def __bool__(self):
+        if self.name[-1] in "aeiou":
+            return False
+        return True
+
+
+class Quadrilateral:
+    def __init__(self,*args):
+        self.width=args[0]
+        self.height=args[-1]
+    def __str__(self):
+        if self.width==self.height:
+            return f"Куб размером {self.width}х{self.height}"
+        else:
+            return f"Прямоугольник размером {self.width}х{self.height}"
+    def __bool__(self):
+        return self.width==self.height
